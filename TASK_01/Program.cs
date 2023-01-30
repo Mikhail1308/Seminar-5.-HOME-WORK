39cbb385d00f705b5d6f6701fb1b9d7fc761b230
Mikhail Nikolaev <armavir1308@mail.ru>
2023-01-30 19:39:14 +0300

    TASK_01. Подсчет четных элементов массива

--- TASK_01/Program.cs
+++ TASK_01/Program.cs
@@ -19,7 +19,6 @@ void PrintArray(int[] array, string msg)
     Console.Write("[");
     for (int i = 0; i < array.Length; i++)
     {
-        //Console.Write($"{array[i]}\t");
         Console.Write(array[i] + "  ");
     }
     Console.Write("]");
